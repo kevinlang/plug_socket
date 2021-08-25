52616c5ba318a0de2c2ce876dec29a00d5022884
Kevin Lang <me@gnalck.com>
2021-08-25 18:29:03 -0400

    update mix, add ci, add license

--- mix.exs
+++ mix.exs
@@ -1,28 +1,44 @@
 defmodule PlugSocket.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @url "https://github.com/kevinlang/plug_socket"
+
   def project do
     [
       app: :plug_socket,
-      version: "0.1.0",
-      elixir: "~> 1.12",
+      version: @version,
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp docs do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      main: "PlugSocket",
+      source_ref: "v#{@version}",
+      source_url: @url
     ]
+  end
+
+  defp package do
+    %{
+      licenses: ["Apache 2"],
+      maintainers: ["Kevin Lang"],
+      links: %{"GitHub" => @url}
+    }
+  end
+
+  defp deps do
+    []
   end
 end
